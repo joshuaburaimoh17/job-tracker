483da012ed536cbb98eb2155dfe9906f07c661a1
Joshua-Buraimoh <joshuaburaimoh17@gmail.com>
2026-05-25 18:16:37 +0100

    Set job queue as the landing page

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.job_queue, name='dashboard'),
     path('applications/', views.application_list, name='application_list'),
     path('applications/<int:pk>/edit/', views.application_edit, name='application_edit'),
     path('applications/<int:pk>/delete/', views.application_delete, name='application_delete'),
